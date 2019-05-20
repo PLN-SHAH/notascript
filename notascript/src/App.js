a1f23f77bb3ef19f228f4872db3b08b2ce3f91c7
PLN-SHAH <41371491+codex415@users.noreply.github.com>
2019-05-20 15:34:01 +0200

    first commit

---
+++ notascript/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  state = {
+    cards: []
+  };
+
+  componentDidMount() {
+    fetch("/cards")
+      .then(res => res.json())
+      .then(data => this.setState({ cards: data }))
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    const { cards } = this.state;
+
+    return (
+      <main>
+        <h1>notascript</h1>
+      </main>
+    );
+  }
+}
