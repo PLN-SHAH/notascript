e2a7e7a49c970cbddf314e3507286385ac5d3006
PLN-SHAH <41371491+codex415@users.noreply.github.com>
2019-05-21 16:51:30 +0200

    finish chunking

--- notascript/src/App.js
+++ notascript/src/App.js
@@ -22,6 +22,7 @@ const StyledOutput = styled.section`
   max-width: 100vw;
   border: 1px solid black;
   word-break: break-all;
+  font-size: 3em;
 `;
 
 export default class App extends Component {
@@ -37,17 +38,11 @@ export default class App extends Component {
       actualSign: content,
       outputStream: [...this.state.outputStream, content]
     });
-  };
-
-  componentDidUpdate() {
-    const outputStream = this.state.outputStream;
-
-    //save every 9 item array in cache
-    if (outputStream.length % 4 === 0) {
+    if (this.state.outputStream.length % 4 === 0) {
       //console.log("outputStream: mod 4  ", outputStream);
-      this.sliceArray(outputStream);
+      this.sliceArray(this.state.outputStream);
     }
-  }
+  };
 
   sliceArray(array) {
     let slicedArray = [];
@@ -56,19 +51,18 @@ export default class App extends Component {
     for (let i = 0; i < array.length; i += 4) {
       slicedArray.push(array.slice(i, i + 4));
     }
+    console.log(slicedArray, "sliced array");
 
-    console.log(slicedArray, "sliced array function");
+    console.log(this.state.outputStream);
+    return slicedArray;
   }
 
   render() {
-    console.log(this.state, "this state");
     return (
       <main>
         <Header />
-        <StyledOutput>
-          outputStream: {this.state.outputStream.length} <br />
-          {this.state.outputStream}
-        </StyledOutput>
+        <div>{this.slicedArray}</div>
+        <StyledOutput>{this.state.outputStream}</StyledOutput>
         <StyledButton onClick={this.handleClick}>&#1049;</StyledButton>
         <StyledButton onClick={this.handleClick}>&#859;</StyledButton>
         <StyledButton onClick={this.handleClick}>&#1029;</StyledButton>
