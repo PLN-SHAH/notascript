05aa74c24ff13aa8e8cb6657115d90eae3a5f04a
PLN-SHAH <41371491+codex415@users.noreply.github.com>
2019-05-22 10:47:43 +0200

    clean up code

--- notascript/src/App.js
+++ notascript/src/App.js
@@ -1,68 +1,63 @@
-import React, { Component } from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import Buttons from "./Buttons";
-import styled from "styled-components";
+import React, { Component } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Buttons from './Buttons';
+import styled from 'styled-components';
 
 const StyledOutput = styled.section`
-  max-width: 100vw;
-  border: 1px solid black;
-  word-break: break-all;
-  font-size: 3em;
+	border: 1px solid black;
+	font-size: 3em;
+	max-width: 100vw;
+	word-break: break-all;
 `;
 
 export default class App extends Component {
-  state = {
-    outputStream: [],
-    actualSign: ""
-  };
+	state = {
+		outputStream: [],
+		actualSign: ''
+	};
 
-  //getButtonValue
-  handleButtonClick(label) {
-    console.log("handleButtonClick", label);
-    const content = label;
+	handleButtonClick(label) {
+		console.log('handleButtonClick', label);
+		const content = label;
 
-    this.setState({
-      actualSign: content,
-      outputStream: [...this.state.outputStream, content]
-    });
-    if (this.state.outputStream.length % 4 === 0) {
-      //console.log("outputStream: mod 4  ", outputStream);
-      this.sliceArray(this.state.outputStream);
-    }
-  }
+		this.setState({
+			actualSign: content,
+			outputStream: [...this.state.outputStream, content]
+		});
+		if (this.state.outputStream.length % 4 === 0) {
+			this.sliceArray(this.state.outputStream);
+		}
+	}
 
-  sliceArray(array) {
-    let slicedArray = [];
+	sliceArray(array) {
+		let slicedArray = [];
 
-    //toDo change 4 dynamically
-    for (let i = 0; i < array.length; i += 4) {
-      slicedArray.push(array.slice(i, i + 4));
-    }
-    console.log(slicedArray, "sliced array");
+		for (let i = 0; i < array.length; i += 4) {
+			slicedArray.push(array.slice(i, 4));
+		}
+		//dev:	console.log(slicedArray, 'sliced array');
+		return slicedArray;
+	}
 
-    console.log(this.state.outputStream);
-    return slicedArray;
-  }
-
-  render() {
-    return (
-      <main>
-        <Header />
-        <StyledOutput>{this.state.outputStream}</StyledOutput>
-        <Buttons
-          buttonLabels={[
-            String.fromCodePoint(0x03a3),
-            String.fromCodePoint(0x03b6),
-            String.fromCodePoint(0x03a6),
-            String.fromCodePoint(0x03a9),
-            String.fromCodePoint(0x03df),
-            String.fromCodePoint(0x03f4)
-          ]}
-          handleButtonClick={label => this.handleButtonClick(label)}
-        />
-        <Footer />
-      </main>
-    );
-  }
+	render() {
+		return (
+			<main>
+				<Header />
+				<StyledOutput>{this.state.outputStream}</StyledOutput>
+				<Buttons
+					buttonLabels={[
+						String.fromCodePoint(0x03a3),
+						String.fromCodePoint(0x03b6),
+						String.fromCodePoint(0x03a6),
+						String.fromCodePoint(0x03a9),
+						String.fromCodePoint(0x03df),
+						String.fromCodePoint(0x03f4)
+					]}
+					handleButtonClick={label => this.handleButtonClick(label)}
+				/>
+				<Footer />
+			</main>
+		);
+	}
 }
--- notascript/src/Buttons.js
+++ notascript/src/Buttons.js
@@ -1,37 +1,36 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 const StyledButtonContainer = styled.section`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-self: end;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-self: end;
 `;
 
 const StyledButton = styled.button`
-  color: white;
-  border: 0;
-  margin-right: 5px;
-  border-bottom: 5px solid #4c4a58;
-  height: 50px;
-  width: 50px;
+	color: $c-main;
+	border: 0;
+	border-bottom: 5px solid #4c4a58;
+	height: 50px;
+	margin-right: 5px;
+	width: 50px;
 
-  &:hover,
-  &:active {
-    border-bottom: 15px solid #4c4a58;
-    font-size: 2.2em;
-  }
+	&:hover,
+	&:active {
+		border-bottom: 15px solid #4c4a58;
+		font-size: 2.2em;
+	}
 `;
 
 export default function Button({ buttonLabels, handleButtonClick }) {
-  console.log("Button");
-  return (
-    <StyledButtonContainer>
-      {buttonLabels.map(label => (
-        <StyledButton key={label} onClick={() => handleButtonClick(label)}>
-          {label}
-        </StyledButton>
-      ))}
-    </StyledButtonContainer>
-  );
+	return (
+		<StyledButtonContainer>
+			{buttonLabels.map(label => (
+				<StyledButton key={label} onClick={() => handleButtonClick(label)}>
+					{label}
+				</StyledButton>
+			))}
+		</StyledButtonContainer>
+	);
 }
