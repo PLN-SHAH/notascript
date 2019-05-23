e6b555e9a5e807ef6c710a8ed63963cdba871c51
PLN-SHAH <41371491+codex415@users.noreply.github.com>
2019-05-23 16:31:59 +0200

    get input from form

--- notascript/src/CreateDocument.js
+++ notascript/src/CreateDocument.js
@@ -1,28 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 const StyledForm = styled.form`
 	display: grid;
 `;
 
-export default class CreateDocument extends Component {
-	render() {
-		return (
-			<>
-				<h3>Create a new file</h3>
-				<StyledForm>
-					<label>New Title</label>
-					<input />
-					<label>New Description</label>
-					<textarea />
-					<label>Choose domains</label>
-					<select name='top5' size='5'>
-						<option>domain option1</option>
-						<option>domain option3</option>
-						<option>domain option3</option>
-					</select>
-				</StyledForm>
-			</>
-		);
+export default function CreateDocument() {
+	function handleOnSubmit(event) {
+		event.preventDefault();
+
+		const form = event.target;
+		const title = form.title.value;
+		const description = form.description.value;
+		console.log(title, description);
 	}
+
+	return (
+		<StyledForm onSubmit={handleOnSubmit}>
+			<label htmlFor='title'>New Title</label>
+			<input name='title' placeholder='type title here' type='text' />
+
+			<label htmlFor='description'>New Description</label>
+			<textarea
+				name='description'
+				placeholder='type description here'
+				type='text'
+			/>
+
+			<label htmlFor='domainSelection'>Choose domains</label>
+			<select name='domainSelection' size='5'>
+				<option>domain option1</option>
+				<option>domain option3</option>
+				<option>domain option3</option>
+			</select>
+			<section>
+				<button>save</button>
+			</section>
+		</StyledForm>
+	);
 }
