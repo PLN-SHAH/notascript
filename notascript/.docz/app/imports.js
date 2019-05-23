9861dbb07b039dd95b275be6b7dc33ea428cc6ea
PLN-SHAH <41371491+codex415@users.noreply.github.com>
2019-05-23 12:00:50 +0200

    add docz and install react router again

--- notascript/.docz/app/imports.js
+++ notascript/.docz/app/imports.js
@@ -1,1 +1,6 @@
-export const imports = {}
+export const imports = {
+  'src/index.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-index" */ 'src/index.mdx'
+    ),
+}
