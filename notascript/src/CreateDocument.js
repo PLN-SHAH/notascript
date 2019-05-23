d3082973c79365e259f43894dc2a6845e1638993
PLN-SHAH <41371491+codex415@users.noreply.github.com>
2019-05-23 16:41:27 +0200

    add form onsubmit handler

--- notascript/src/CreateDocument.js
+++ notascript/src/CreateDocument.js
@@ -1,41 +1,10 @@
 import React from 'react';
-import styled from 'styled-components';
+import Form from './Form';
 
-const StyledForm = styled.form`
-	display: grid;
-`;
-
-export default function CreateDocument() {
-	function handleOnSubmit(event) {
-		event.preventDefault();
-
-		const form = event.target;
-		const title = form.title.value;
-		const description = form.description.value;
-		console.log(title, description);
-	}
-
+export default function CreateDocument({ onFormSubmit }) {
 	return (
-		<StyledForm onSubmit={handleOnSubmit}>
-			<label htmlFor='title'>New Title</label>
-			<input name='title' placeholder='type title here' type='text' />
-
-			<label htmlFor='description'>New Description</label>
-			<textarea
-				name='description'
-				placeholder='type description here'
-				type='text'
-			/>
-
-			<label htmlFor='domainSelection'>Choose domains</label>
-			<select name='domainSelection' size='5'>
-				<option>domain option1</option>
-				<option>domain option3</option>
-				<option>domain option3</option>
-			</select>
-			<section>
-				<button>save</button>
-			</section>
-		</StyledForm>
+		<>
+			<Form onFormSubmit={onFormSubmit} />
+		</>
 	);
 }
---
+++ notascript/src/Form.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledForm = styled.form`
+	display: grid;
+`;
+
+export default function CreateDocument({ onFormSubmit }) {
+	function handleOnSubmit(event) {
+		event.preventDefault();
+
+		const form = event.target;
+		const title = form.title.value;
+		const description = form.description.value;
+
+		onFormSubmit({
+			title,
+			description
+		});
+		console.log(title, description);
+	}
+
+	return (
+		<StyledForm onSubmit={handleOnSubmit}>
+			<label htmlFor='title'>New Title</label>
+			<input name='title' placeholder='type title here' type='text' />
+
+			<label htmlFor='description'>New Description</label>
+			<textarea
+				name='description'
+				placeholder='type description here'
+				type='text'
+			/>
+
+			<label htmlFor='domainSelection'>Choose domains</label>
+			<select name='domainSelection' size='5'>
+				<option>domain option1</option>
+				<option>domain option3</option>
+				<option>domain option3</option>
+			</select>
+			<section>
+				<button>save</button>
+			</section>
+		</StyledForm>
+	);
+}
