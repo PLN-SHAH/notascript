750bb51533fc4e34a3a749c49f19b334bf87bf75
PLN-SHAH <41371491+codex415@users.noreply.github.com>
2019-05-23 14:24:55 +0200

    show file list raw

--- notascript/src/Document.js
+++ notascript/src/Document.js
@@ -1,5 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledDocument = styled.section`
+	border: 1px solid blue;
+	padding: 20px;
+	display: grid;
+	grid-template-columns: auto 100px;
+`;
+
+const StyledTitle = styled.h4`
+	font-size: 2em;
+	margin: 0;
+`;
 
 export default function Document({ document }) {
-	return <div>{document.description}</div>;
+	return (
+		<StyledDocument>
+			<section>
+				<StyledTitle>{document.title}</StyledTitle>
+				<p>{document.description}</p>
+				<ul>
+					<li>domain</li>
+					<li>domain</li>
+				</ul>
+			</section>
+			<section>
+				<button>edit</button>
+				<button>delete</button>
+			</section>
+		</StyledDocument>
+	);
 }
