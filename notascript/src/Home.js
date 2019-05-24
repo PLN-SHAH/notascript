32dcebb9d23a19c74f884ff171849f3ed67ebe83
PLN-SHAH <41371491+codex415@users.noreply.github.com>
2019-05-24 10:31:53 +0200

    code clean Home

--- notascript/src/Home.js
+++ notascript/src/Home.js
@@ -5,27 +5,23 @@ import NavLink from './NavLink.js';
 const StyledNavigation = styled.nav`
 	font-size: 3em;
 	display: grid;
-	justify-content: center;
 	align-items: center;
 	height: 100%;
-	grid-template-rows: repeat(3, 33%);
+	padding: 20px;
 `;
 
-const StyledNavLinkContainer = styled.a`
+const StyledNavLinkContainer = styled.button`
 	background: linear-gradient(135deg, #562323, #4c4a58);
 	padding: 20px;
-	width: 500px;
+	width: 100%;
 	text-align: center;
-	margin-bottom: 30px;
+	font-family: 'Raleway', sans-serif;
 `;
 
 export default class Home extends Component {
 	render() {
 		return (
 			<StyledNavigation>
-				<StyledNavLinkContainer>
-					<NavLink to='/home'>Home</NavLink>
-				</StyledNavLinkContainer>
 				<StyledNavLinkContainer>
 					<NavLink to='/work'>Work</NavLink>
 				</StyledNavLinkContainer>
