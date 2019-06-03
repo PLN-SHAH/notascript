160daa198b5bca051e0edae1ff738614f885efe6
PLN-SHAH <41371491+codex415@users.noreply.github.com>
2019-06-03 10:27:07 +0200

    finish dictionary styling

--- notascript/src/dictionary/Dictionary.js
+++ notascript/src/dictionary/Dictionary.js
@@ -3,16 +3,24 @@ import NavLink from '../app/NavLink.js';
 import styled from 'styled-components';
 import PropType from 'prop-types';
 import { Title } from '../misc/Style.js';
+import ReactSVG from 'react-svg';
 
 const StyledTitle = styled(Title)``;
+
+const StyledNavLink = styled(NavLink)`
+	grid-template-columns: 50% 50%;
+	text-align: left;
+	font-size: 1em;
+	justify-content: space-between;
+`;
 
 export default function Dictionary({ dictionary }) {
 	const { title, id } = dictionary;
 	return (
-		<>
+		<StyledNavLink to={`/editDictionary/${id}`}>
 			<StyledTitle>{title}</StyledTitle>
-			<NavLink to={`/editDictionary/${id}`}>Edit</NavLink>
-		</>
+			<ReactSVG src='icon-edit.svg' />
+		</StyledNavLink>
 	);
 }
 
--- notascript/src/dictionary/DictionaryList.js
+++ notascript/src/dictionary/DictionaryList.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import PropType from 'prop-types';
 import Dictionary from './Dictionary.js';
+import styled from 'styled-components';
+import { Title } from '../misc/Style.js';
+
+const StyledTitle = styled(Title)`
+	text-align: center;
+`;
 
 export default function DictionaryList({ dictionaries }) {
 	console.log(dictionaries, 'dictionaries');
 	return (
 		<>
+			<StyledTitle>Dictionaries</StyledTitle>
 			{dictionaries.map(dictionary => (
 				<Dictionary dictionary={dictionary} key={dictionary.id} />
 			))}
