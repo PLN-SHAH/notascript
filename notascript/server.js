fb134b3e8b213345c75cb96fd0fe5c249c8d212f
PLN-SHAH <41371491+codex415@users.noreply.github.com>
2019-06-05 15:58:44 +0200

    configure mongodb

--- notascript/server.js
+++ notascript/server.js
@@ -1,5 +1,12 @@
-const setupServer = require('./setup-server');
-const app = setupServer();
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'build')));
 
 const Document = require('./models/Document');
 
@@ -15,3 +22,23 @@ app.post('/documents', (req, res) => {
 		.catch(err => console.error(err));
 	console.log('Successful Document Update');
 });
+
+const db = 'mongodb://localhost:27017/notascript';
+const port = process.env.PORT || 4000;
+
+mongoose
+	.connect(db, { useNewUrlParser: true })
+	.then(() => console.log('Connected to MongoDB'))
+	.catch(err => console.error(err));
+
+app.listen(port, err => {
+	err ? console.log(err) : console.log(`Server ready on port ${port}`);
+});
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('build'));
+
+	app.get('*', function(req, res) {
+		res.sendFile(path.join(__dirname, 'build', 'index.html'));
+	});
+}
--- notascript/src/create/CreateDocument.js
+++ notascript/src/create/CreateDocument.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import CreateForm from './CreateForm.js';
 import PropType from 'prop-types';
 
