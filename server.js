6f374794ef23f421293a3444e0e74994e4276169
PLN-SHAH <41371491+codex415@users.noreply.github.com>
2019-06-17 16:10:09 +0200

    .env config var

--- server.js
+++ server.js
@@ -1,90 +1,90 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'build')))
 
-const Document = require('./models/Document');
-const Dictionary = require('./models/Dictionary');
+const Document = require('./models/Document')
+const Dictionary = require('./models/Dictionary')
 
 app.get('/documents', (req, res) => {
-	Document.find()
-		.then(document => res.json(document))
-		.catch(err => res.json(err));
-});
+  Document.find()
+    .then(document => res.json(document))
+    .catch(err => res.json(err))
+})
 
 app.get('/dictionaries', (req, res) => {
-	Dictionary.find()
-		.then(dictionary => res.json(dictionary))
-		.catch(err => res.json(err));
-});
+  Dictionary.find()
+    .then(dictionary => res.json(dictionary))
+    .catch(err => res.json(err))
+})
 
 app.post('/documents', (req, res) => {
-	Document.create(req.body)
-		.then(document => res.status(201).json(document))
-		.catch(err => console.error(err));
-	console.log('Successful Document Update');
-});
+  Document.create(req.body)
+    .then(document => res.status(201).json(document))
+    .catch(err => console.error(err))
+  console.log('Successful Document Update')
+})
 
 app.post('/dictionaries', (req, res) => {
-	Dictionary.create(req.body)
-		.then(dictionary => res.status(201).json(dictionary))
-		.catch(err => console.error(err));
-	console.log('Successful Document Update');
-});
+  Dictionary.create(req.body)
+    .then(dictionary => res.status(201).json(dictionary))
+    .catch(err => console.error(err))
+  console.log('Successful Document Update')
+})
 
 app.delete('/documents/:id', (req, res) => {
-	const { id } = req.params;
-	Document.findByIdAndDelete(id)
-		.then(document => res.status(200).json(document))
-		.catch(err => res.status(500).json(err));
-});
+  const { id } = req.params
+  Document.findByIdAndDelete(id)
+    .then(document => res.status(200).json(document))
+    .catch(err => res.status(500).json(err))
+})
 
 app.delete('/dictionaries/:id', (req, res) => {
-	const { id } = req.params;
-	Dictionary.findByIdAndDelete(id)
-		.then(dictionary => res.status(200).json(dictionary))
-		.catch(err => res.status(500).json(err));
-});
+  const { id } = req.params
+  Dictionary.findByIdAndDelete(id)
+    .then(dictionary => res.status(200).json(dictionary))
+    .catch(err => res.status(500).json(err))
+})
 
 app.patch('/documents/:id', (req, res) => {
-	const { id } = req.params;
-	const { title, description, symbols } = req.body;
+  const { id } = req.params
+  const { title, description, symbols } = req.body
 
-	Document.findByIdAndUpdate(id, { title, description, symbols }, { new: true })
-		.then(document => res.json(document))
-		.catch(err => err.json({ errors: [err] }));
-});
+  Document.findByIdAndUpdate(id, { title, description, symbols }, { new: true })
+    .then(document => res.json(document))
+    .catch(err => err.json({ errors: [err] }))
+})
 
 app.patch('/dictionaries/:id', (req, res) => {
-	const { id } = req.params;
-	const { synonym, meaning } = req.body;
-	Dictionary.findByIdAndUpdate(id, { synonym, meaning }, { new: true })
-		.then(dictionary => res.json(dictionary))
-		.catch(err => err.json({ errors: [err] }));
-});
+  const { id } = req.params
+  const { synonym, meaning } = req.body
+  Dictionary.findByIdAndUpdate(id, { synonym, meaning }, { new: true })
+    .then(dictionary => res.json(dictionary))
+    .catch(err => err.json({ errors: [err] }))
+})
 
 //CONFIGURATION
-const db = 'mongodb://localhost:27017/notascript';
-const port = process.env.PORT || 4000;
+const db = process.env.DB_URL || 'mongodb://localhost:27017/notascript'
+const port = process.env.PORT || 4000
 
 mongoose
-	.connect(db, { useNewUrlParser: true })
-	.then(() => console.log('Connected to MongoDB'))
-	.catch(err => console.error(err));
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error(err))
 
 app.listen(port, err => {
-	err ? console.log(err) : console.log(`Server ready on port ${port}`);
-});
+  err ? console.log(err) : console.log(`Server ready on port ${port}`)
+})
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('build'));
+  app.use(express.static('build'))
 
-	app.get('*', function(req, res) {
-		res.sendFile(path.join(__dirname, 'build', 'index.html'));
-	});
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  })
 }
