a26ae63e1020598c224b4d1f2e119a4652f95bd1
PLN-SHAH <41371491+codex415@users.noreply.github.com>
2019-06-18 11:49:19 +0200

    font-weight add

--- src/app/Header.js
+++ src/app/Header.js
@@ -1,34 +1,34 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
 const StyledHeader = styled.header`
-	background-color: #373f43;
-	color: white;
-	display: grid;
-	align-items: center;
-	box-shadow: 3px 3px 5px 2px #ccc;
-`;
+  background-color: #373f43;
+  color: white;
+  display: grid;
+  align-items: center;
+  box-shadow: 3px 3px 5px 2px #ccc;
+`
 
 const StyledTitle = styled.h1`
-	font-size: 1em;
-	text-align: center;
-`;
+  font-size: 1em;
+  text-align: center;
+`
 const StyledNota = styled.span`
-	font-family: 'Raleway', sans-serif;
-	font-weight: bold;
-`;
+  font-family: 'Raleway', sans-serif;
+  font-weight: bold;
+`
 
 const StyledScript = styled.span`
-	font-family: 'Dancing Script', cursive;
-`;
+  font-family: 'Dancing Script', cursive;
+`
 
 export default function Header() {
-	return (
-		<StyledHeader>
-			<StyledTitle>
-				<StyledNota>Nota</StyledNota>
-				<StyledScript>script</StyledScript>
-			</StyledTitle>
-		</StyledHeader>
-	);
+  return (
+    <StyledHeader>
+      <StyledTitle>
+        <StyledNota>Nota</StyledNota>
+        <StyledScript>script</StyledScript>
+      </StyledTitle>
+    </StyledHeader>
+  )
 }
