02348e8ff07f5c4602a96a5ace59fd270b669939
PLN-SHAH <41371491+codex415@users.noreply.github.com>
2019-06-19 13:03:53 +0200

    fix

--- src/app/App.js
+++ src/app/App.js
@@ -1,227 +1,227 @@
-import DictionaryAdd from '../components/dictionary/DictionaryAdd.js';
-import DocumentCreate from '../components/create/Document';
-import DictionaryList from '../components/dictionary/DictionaryList.js';
-import Details from '../components/document/Details.js';
-import Documents from '../components/document/Documents.js';
-import Edit from '../components/edit/Edit.js';
-import Footer from './Footer.js';
-import Header from './Header.js';
-import OverviewPage from '../components/overview/OverviewPage.js';
-import React, { useEffect, useState } from 'react';
-import Work from '../components/work/Work.js';
-import styled from 'styled-components';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import { getIndex } from '../services/Services.js';
+import DictionaryAdd from '../components/dictionary/DictionaryAdd.js'
+import DocumentCreate from '../components/create/Document'
+import DictionaryList from '../components/dictionary/DictionaryList.js'
+import Details from '../components/document/Details.js'
+import Documents from '../components/document/Documents.js'
+import Edit from '../components/edit/Edit.js'
+import Footer from './Footer.js'
+import Header from './Header.js'
+import OverviewPage from '../components/overview/OverviewPage.js'
+import React, { useEffect, useState } from 'react'
+import Work from '../components/work/Work.js'
+import styled from 'styled-components'
+import { Route, Switch, Redirect } from 'react-router-dom'
+import { getIndex } from '../services/Services.js'
 import {
-	deleteDocument,
-	getDocuments,
-	patchDocument,
-	postDocument
-} from '../services/ServiceDocument.js';
+  deleteDocument,
+  getDocuments,
+  patchDocument,
+  postDocument,
+} from '../services/ServiceDocument.js'
 
 import {
-	deleteDictionary,
-	getDictionaries,
-	postDictionary
-} from '../services/ServiceDictionary.js';
+  deleteDictionary,
+  getDictionaries,
+  postDictionary,
+} from '../services/ServiceDictionary.js'
 
 const StyledContent = styled.section`
-	overflow-y: scroll;
-	word-break: break-all;
-	width: 100vw;
-`;
+  overflow-y: scroll;
+  word-break: break-all;
+  width: 100vw;
+`
 
 export default function App() {
-	const [documents, setDocuments] = useState([]);
-	const [dictionaries, setDictionaries] = useState([]);
+  const [documents, setDocuments] = useState([])
+  const [dictionaries, setDictionaries] = useState([])
 
-	useEffect(() => {
-		loadData();
-	}, []);
+  useEffect(() => {
+    loadData()
+  }, [])
 
-	function loadData() {
-		getDocuments()
-			.then(data => {
-				setDocuments(data);
-			})
-			.catch(error => console.log(error));
+  function loadData() {
+    getDocuments()
+      .then(data => {
+        setDocuments(data)
+      })
+      .catch(error => console.log(error))
 
-		getDictionaries()
-			.then(data => {
-				setDictionaries(data);
-			})
-			.catch(error => console.log(error));
-	}
+    getDictionaries()
+      .then(data => {
+        setDictionaries(data)
+      })
+      .catch(error => console.log(error))
+  }
 
-	const createDocument = (data, history) => {
-		postDocument(data)
-			.then(newDocument => {
-				setDocuments([newDocument, ...documents]);
-				history.push('/');
-			})
-			.catch(error => console.log(error));
-	};
+  const createDocument = (data, history) => {
+    postDocument(data)
+      .then(newDocument => {
+        setDocuments([newDocument, ...documents])
+        history.push('/')
+      })
+      .catch(error => console.log(error))
+  }
 
-	const createDictionary = (data, history) => {
-		postDictionary(data)
-			.then(newDictionary => {
-				setDictionaries([...dictionaries, newDictionary]);
-				history.push('/');
-			})
-			.catch(error => console.log(error));
-	};
+  const createDictionary = (data, history) => {
+    postDictionary(data)
+      .then(newDictionary => {
+        setDictionaries([...dictionaries, newDictionary])
+        history.push('/')
+      })
+      .catch(error => console.log(error))
+  }
 
-	const readData = (props, dataList) => {
-		return dataList.filter(data => data._id === props.match.params.id)[0];
-	};
+  const readData = (props, dataList) => {
+    return dataList.filter(data => data._id === props.match.params.id)[0]
+  }
 
-	const updateDocument = document => {
-		const { title, description, symbols, _id } = document;
-		const index = getIndex(documents, document);
+  const updateDocument = document => {
+    const { title, description, symbols, _id } = document
+    const index = getIndex(documents, document)
 
-		patchDocument(document, document._id);
+    patchDocument(document, document._id)
 
-		const updatedDocument = {
-			title,
-			description,
-			symbols,
-			_id
-		};
+    const updatedDocument = {
+      title,
+      description,
+      symbols,
+      _id,
+    }
 
-		setDocuments([
-			...documents.slice(0, index),
-			updatedDocument,
-			...documents.slice(index + 1)
-		]);
-	};
+    setDocuments([
+      ...documents.slice(0, index),
+      updatedDocument,
+      ...documents.slice(index + 1),
+    ])
+  }
 
-	const removeDocument = document => {
-		const index = getIndex(documents, document);
+  const removeDocument = document => {
+    const index = getIndex(documents, document)
 
-		deleteDocument(document, document._id);
+    deleteDocument(document, document._id)
 
-		setDocuments([...documents.slice(0, index), ...documents.slice(index + 1)]);
-	};
+    setDocuments([...documents.slice(0, index), ...documents.slice(index + 1)])
+  }
 
-	const removeDictionary = dictionary => {
-		const index = getIndex(dictionaries, dictionary);
+  const removeDictionary = dictionary => {
+    const index = getIndex(dictionaries, dictionary)
 
-		deleteDictionary(dictionary, dictionary._id);
+    deleteDictionary(dictionary, dictionary._id)
 
-		setDictionaries([
-			...dictionaries.slice(0, index),
-			...dictionaries.slice(index + 1)
-		]);
-	};
+    setDictionaries([
+      ...dictionaries.slice(0, index),
+      ...dictionaries.slice(index + 1),
+    ])
+  }
 
-	const createDictionaryEntry = dictionary => {
-		const { _id, entries, title } = dictionary;
+  const createDictionaryEntry = dictionary => {
+    const { _id, entries, title } = dictionary
 
-		const index = getIndex(dictionaries, dictionary);
+    const index = getIndex(dictionaries, dictionary)
 
-		const updatedDictionary = {
-			title,
-			entries: [...entries],
-			_id
-		};
-		//patchDictionary(dictionary, dictionary._id);
+    const updatedDictionary = {
+      title,
+      entries: [...entries],
+      _id,
+    }
+    //patchDictionary(dictionary, dictionary._id);
 
-		setDictionaries([
-			...dictionaries.slice(0, index),
-			updatedDictionary,
-			...dictionaries.slice(index + 1)
-		]);
-	};
+    setDictionaries([
+      ...dictionaries.slice(0, index),
+      updatedDictionary,
+      ...dictionaries.slice(index + 1),
+    ])
+  }
 
-	return (
-		<main>
-			<Header />
-			<StyledContent>
-				<Switch>
-					<Route
-						path='/create'
-						render={props => (
-							<DocumentCreate
-								onFormSubmit={data => createDocument(data)}
-								{...props}
-							/>
-						)}
-					/>
-					<Route
-						path='/dictionaries'
-						render={() => (
-							<DictionaryList
-								onFormSubmit={data => createDictionary(data)}
-								onDelete={document => removeDictionary(document)}
-								dictionaries={dictionaries}
-							/>
-						)}
-					/>
-					<Route
-						path='/documents'
-						render={props => (
-							<Documents
-								onDelete={document => {
-									removeDocument(document);
-								}}
-								documents={documents}
-								{...props}
-							/>
-						)}
-					/>
+  return (
+    <main>
+      <Header />
+      <StyledContent>
+        <Switch>
+          <Route
+            path="/create"
+            render={props => (
+              <DocumentCreate
+                onFormSubmit={data => createDocument(data)}
+                {...props}
+              />
+            )}
+          />
+          <Route
+            path="/dictionaries"
+            render={() => (
+              <DictionaryList
+                onFormSubmit={data => createDictionary(data)}
+                onDelete={document => removeDictionary(document)}
+                dictionaries={dictionaries}
+              />
+            )}
+          />
+          <Route
+            path="/documents"
+            render={props => (
+              <Documents
+                onDelete={document => {
+                  removeDocument(document)
+                }}
+                documents={documents}
+                {...props}
+              />
+            )}
+          />
 
-					<Route
-						path='/details/:id'
-						render={props => (
-							<Details selectedDocument={readData(props, documents)} />
-						)}
-					/>
-					<Route
-						path='/edit/:id'
-						render={props => (
-							<Edit
-								onFormSubmit={document => updateDocument(document)}
-								selectedDocument={readData(props, documents)}
-								{...props}
-							/>
-						)}
-					/>
+          <Route
+            path="/details/:id"
+            render={props => (
+              <Details selectedDocument={readData(props, documents)} />
+            )}
+          />
+          <Route
+            path="/edit/:id"
+            render={props => (
+              <Edit
+                onFormSubmit={document => updateDocument(document)}
+                selectedDocument={readData(props, documents)}
+                {...props}
+              />
+            )}
+          />
 
-					<Route
-						path='/dictionariesE/:id'
-						render={props => (
-							<DictionaryAdd
-								onFormSubmitEntries={dictionary =>
-									createDictionaryEntry(dictionary)
-								}
-								dictionary={readData(props, dictionaries)}
-							/>
-						)}
-					/>
+          <Route
+            path="/dictionaries/:id"
+            render={props => (
+              <DictionaryAdd
+                onFormSubmitEntries={dictionary =>
+                  createDictionaryEntry(dictionary)
+                }
+                dictionary={readData(props, dictionaries)}
+              />
+            )}
+          />
 
-					<Route
-						path='/work/:id'
-						render={props => (
-							<Work
-								onFormSubmitEntries={dictionary =>
-									createDictionaryEntry(dictionary)
-								}
-								dictionaries={dictionaries}
-								selectedDocument={readData(props, documents)}
-								updateDocumentSymbols={selectedDocument =>
-									updateDocument(selectedDocument)
-								}
-								{...props}
-							/>
-						)}
-					/>
+          <Route
+            path="/work/:id"
+            render={props => (
+              <Work
+                onFormSubmitEntries={dictionary =>
+                  createDictionaryEntry(dictionary)
+                }
+                dictionaries={dictionaries}
+                selectedDocument={readData(props, documents)}
+                updateDocumentSymbols={selectedDocument =>
+                  updateDocument(selectedDocument)
+                }
+                {...props}
+              />
+            )}
+          />
 
-					<Route path='/overview' render={props => <OverviewPage />} />
-					<Redirect from='/' to='/documents' />
-					<Route exact path='/not-found' component={() => <h1>Not Found</h1>} />
-				</Switch>
-			</StyledContent>
-			<Footer />
-		</main>
-	);
+          <Route path="/overview" render={props => <OverviewPage />} />
+          <Redirect from="/" to="/documents" />
+          <Route exact path="/not-found" component={() => <h1>Not Found</h1>} />
+        </Switch>
+      </StyledContent>
+      <Footer />
+    </main>
+  )
 }
--- src/components/dictionary/Dictionary.js
+++ src/components/dictionary/Dictionary.js
@@ -1,55 +1,55 @@
-import NavLink from '../../app/NavLink.js';
-import PropType from 'prop-types';
-import React from 'react';
-import styled from 'styled-components';
+import NavLink from '../../app/NavLink.js'
+import PropType from 'prop-types'
+import React from 'react'
+import styled from 'styled-components'
 
 const StyledContainer = styled.section`
-	display: grid;
-	align-items: center;
-	grid-template-columns: repeat(2, 1fr);
-	padding: 0 20px;
-`;
+  display: grid;
+  align-items: center;
+  grid-template-columns: repeat(2, 1fr);
+  padding: 0 20px;
+`
 
 const StyledTitle = styled.h4`
-	font-weight: bold;
-	color: #2eadd3;
-`;
+  font-weight: bold;
+  color: #2eadd3;
+`
 const StyledIcons = styled.section`
-	text-align: right;
+  text-align: right;
 
-	i {
-		color: #373f43;
-	}
-`;
+  i {
+    color: #373f43;
+  }
+`
 
 const StyledButton = styled.button`
-	font-size: 1em;
-`;
+  font-size: 1em;
+`
 
 export default function Dictionary({ dictionary, onDelete }) {
-	const { title, _id } = dictionary;
+  const { title, _id } = dictionary
 
-	return (
-		<StyledContainer>
-			<NavLink to={`/dictionariesE/${_id}`}>
-				<StyledTitle>{title}</StyledTitle>
-			</NavLink>
-			<StyledIcons>
-				<NavLink to={`/dictionariesE/${_id}`}>
-					<i className='far fa-edit' />
-				</NavLink>
-				<StyledButton onClick={onDelete}>
-					<i className='fas fa-trash-alt' />
-				</StyledButton>
-			</StyledIcons>
-		</StyledContainer>
-	);
+  return (
+    <StyledContainer>
+      <NavLink to={`/dictionaries/${_id}`}>
+        <StyledTitle>{title}</StyledTitle>
+      </NavLink>
+      <StyledIcons>
+        <NavLink to={`/dictionaries/${_id}`}>
+          <i className="far fa-edit" />
+        </NavLink>
+        <StyledButton onClick={onDelete}>
+          <i className="fas fa-trash-alt" />
+        </StyledButton>
+      </StyledIcons>
+    </StyledContainer>
+  )
 }
 
 Dictionary.propType = {
-	dictionary: PropType.shape({
-		_id: PropType.number,
-		title: PropType.string.isRequired
-	}),
-	onDelete: PropType.func
-};
+  dictionary: PropType.shape({
+    _id: PropType.number,
+    title: PropType.string.isRequired,
+  }),
+  onDelete: PropType.func,
+}
